576eb567ed92b478a30b467a0590fb97c646532b
linxunke <linx15@EC212-ECWS-14.coventry.ac.uk>
2015-10-30 12:40:36 +0000

    s
    d

---
+++ Translator/Translator/App_Code/TranslatorClass.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Translator.App_Code
+{
+    public class TranslatorClass
+    {
+        string mstrText, mstrOriginal;
+        const string HM = "hey mate!";
+        public string Text
+        {
+            get
+            {
+                return mstrText;
+            }
+            set
+            {
+                mstrText = value;
+                mstrOriginal = value;
+            }
+        }
+        public void Restore()
+        {
+            mstrText = mstrOriginal;
+        }
+        public void Translate()
+        {
+            string strWord;
+            string[] arrWords;
+            bool bCaps = false;
+            arrWords = mstrText.Split(' ');
+            for (int intCount = 0; intCount <= arrWords.GetUpperBound(0);
+                intCount++)
+            {
+                strWord = arrWords[intCount].ToLower();
+                if (!arrWords[intCount].Equals(strWord)) bCaps = ture;
+                if (strWord != "")
+                {
+                    strWord = strWord.Substring(1, strWord.Length - 1) +
+                        strWord.Substring(0, 1) + HM;
+                    if (bCaps) strWord = strWord.Substring(0, 1).ToUpper() +
+                          strWord.Substring(1, strWord.Length - 1);
+                }
+                arrWords[intCount] = strWord;
+                bCaps = false;
+            }
+            mstrText = String.Join(" ", arrWords);
+        }
+    }
+}
---
+++ Translator/Translator/Translator.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Translator.aspx.cs" Inherits="Translator.Translator" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        #form1 {
+            height: 103px;
+        }
+    </style>
+</head>
+<body style="height: 508px">
+    <form id="form1" runat="server">
+        <asp:Label ID="Label1" runat="server" Text="Universal Translator:)" Font-Bold="True" Font-Size="Large" ForeColor="Black"></asp:Label>
+        <p style="width: 1096px; height: 18px">
+            <asp:TextBox ID="TextBox1" runat="server" Height="34px" TextMode="MultiLine" Width="105px"></asp:TextBox>
+        </p>
+        <p style="width: 1096px; height: 22px">
+            <asp:Button ID="Button1" runat="server" style="margin-bottom: 0px" Text="Translate:)" />
+        </p>
+    </form>
+</body>
+</html>
